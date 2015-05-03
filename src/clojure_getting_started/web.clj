aa8b2c51f227bdee36fb70d43c8f9f46276d5d68
Yosuke MIYAJIMA <mayo.suke@gmail.com>
2015-05-04 08:27:28 +0900

    Correct content-type for splash

--- src/clojure_getting_started/web.clj
+++ src/clojure_getting_started/web.clj
@@ -11,7 +11,7 @@
 
 (defn splash []
   {:status 200
-   :headers {"Content-Type" "text/plain"}
+   :headers {"Content-Type" "text/html"}
    :body (for [kind ["camel" "snake" "kebab"]]
            (format "<a href=\"/%s?input=%s\">%s %s</a><br />"
                    kind sample kind sample))})
